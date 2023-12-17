980f4513619d408b2d12c4d0b8e8da97ba38328f
senkenn <senken32@gmail.com>
2023-12-17 19:28:57 +0900

    show todo list and refresh

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,4 @@
 import * as child_process from "child_process";
-import * as fs from "fs";
-import * as path from "path";
 import * as vscode from "vscode";
 
 const log = function <T>(this: { [key: string]: T }) {
@@ -10,38 +8,39 @@ const log = function <T>(this: { [key: string]: T }) {
 };
 
 export function activate(context: vscode.ExtensionContext) {
-	// const rootPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
-	const rootPath = "/home/senken/personal/vsce-base/todo-list-for-teams";
+	const rootPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
 	log.call({ rootPath });
 	if (!rootPath) {
 		return;
 	}
 
-	vscode.window.registerTreeDataProvider(
-		"todo-list-for-teams",
-		new TodoListProvider(rootPath as string),
+	const todoListProvider = new TodoListProvider(
+		rootPath,
+		context.workspaceState,
 	);
-	vscode.window.createTreeView("todo-list-for-teams", {
-		treeDataProvider: new TodoListProvider(rootPath as string),
-		// showCollapseAll: true,
-	});
 
 	const todoList = [
-		// ...getUnCommitTodoList(rootPath),
-		...getCommittedTodoList(rootPath),
+		// ...todoListProvider.getUnCommitTodoList(),
+		...todoListProvider.getCommittedTodoList(),
 	];
-	log.call({ todoList }); // Fix: Pass the correct 'this' context to logger.call
 
-	// const onSave = vscode.workspace.onDidSaveTextDocument((e) => {
-	// 	updateTreeView(provider);
-	// });
+	// TODO: Is this needed?
+	vscode.window.registerTreeDataProvider(
+		"todo-list-for-teams",
+		todoListProvider,
+	);
+	vscode.window.createTreeView("todo-list-for-teams", {
+		treeDataProvider: todoListProvider,
+	});
+	vscode.commands.registerCommand("nodeDependencies.refreshEntry", () =>
+		todoListProvider.refresh(),
+	);
+	context.workspaceState.update("todoList", todoList);
 
 	const disposable = vscode.commands.registerCommand(
-		"todo-list-for-teams.helloWorld",
+		"todo-list-for-teams.refresh",
 		async () => {
-			// Save and View
-			// TODO: implement
-			context.workspaceState.update("todoList", todoList);
+			todoListProvider.refresh();
 		},
 	);
 
@@ -56,6 +55,7 @@ type UnCommitTodoList = {
 	sourcePath: string;
 	line: number;
 	preview: string;
+	isPreview: boolean;
 }[];
 
 type CommittedTodoList = (UnCommitTodoList[0] & {
@@ -64,175 +64,156 @@ type CommittedTodoList = (UnCommitTodoList[0] & {
 	author: string;
 })[];
 
-class NotContainSearchWordError extends Error {
+class ShouldHaveBeenIncludedSearchWordError extends Error {
 	constructor(message: string) {
-		super(`${NotContainSearchWordError.name}: ${message}`);
-		this.name = "NotContainSearchWordError";
+		super(`${ShouldHaveBeenIncludedSearchWordError.name}: ${message}`);
+		this.name = "ShouldHaveBeenIncludedSearchWordError";
 	}
 }
 
-function getUnCommitTodoList(repoAbsPath: string): UnCommitTodoList {
-	const cmd = `git -C ${repoAbsPath} diff \
-									 | grep -E ${searchWordGit.source} | grep '^+'`;
-	// +                       // TODO: implement
-	// +       // HACK: refactor
+export class TodoListProvider implements vscode.TreeDataProvider<TodoTreeItem> {
+	private currentBranch = "";
 
-	const output = child_process.execSync(cmd).toString();
+	constructor(
+		private workspaceRoot: string,
+		private workspaceState: vscode.ExtensionContext["workspaceState"],
+	) {
+		const stdout = child_process
+			.execSync(`cd ${this.workspaceRoot} && git branch --show-current`)
+			.toString();
 
-	// cut last "\n" and split by "\n"
-	const searchResultArray = output.slice(0, -1).split("\n");
-	const result = searchResultArray.map((output) => {
-		const matchedWord = output.match(searchWordTS);
-		if (!matchedWord) {
-			throw new NotContainSearchWordError(output);
-		}
-		return {
-			prefix: matchedWord[0].slice(0, -1),
-			sourcePath: output.slice(1),
-			line: 0,
-			preview: "",
-		};
-	});
-	return result;
-}
-
-function getCommittedTodoList(repoAbsPath: string): CommittedTodoList {
-	const cmd = `cd ${repoAbsPath} &&
-							 git -c grep.lineNumber=true grep -E ${searchWordGit.source} $(git branch --format='%(objectname) %(refname:short)' \
-							 | sort | uniq -w 40 | cut -c 42-) \
-							 || [ $? = 1 ] # if grep return 1, then return 0`;
-	const gitGrepResult = child_process.execSync(cmd).toString();
-	if (gitGrepResult === "") {
-		return [];
+		// cut last "\n" and set to currentBranch
+		this.currentBranch = stdout.slice(0, -1);
+		log.call({ currentBranch: this.currentBranch });
 	}
 
-	// HACK: refactor
-	// cut last "\n" and split by "\n"
-	const searchResultArray = gitGrepResult.slice(0, -1).split("\n");
-
-	const committedTodoList = searchResultArray.map((output) => {
-		// tracked file
-		const firstIndex = output.indexOf(":");
-		const secondIndex = output.indexOf(":", firstIndex + 1);
-		const thirdIndex = output.indexOf(":", secondIndex + 1);
-		const fullLine = output.slice(thirdIndex + 1);
-		const matchedWord = fullLine.match(searchWordTS);
-		if (!matchedWord) {
-			throw new NotContainSearchWordError(output);
-		}
-		const prefix = matchedWord[0].slice(0, -1);
-		const branch = output.slice(0, firstIndex);
-		const sourcePath = output.slice(firstIndex + 1, secondIndex);
-		const line = Number(output.slice(secondIndex + 1, thirdIndex));
-		const preview = fullLine.slice(matchedWord.index);
-
-		const cmd = `cd ${repoAbsPath} && git blame ${branch} -L ${line},${line} ${sourcePath}`;
-		const blameOutput = child_process.execSync(cmd).toString();
-
-		// get commit hash, author
-		const commitHash = blameOutput.slice(0, 8);
-		const author = blameOutput.split(" ")[1].slice(1);
-
-		// const commitHash = blameOutput.slice(0, 40);
-		return { prefix, branch, sourcePath, line, preview, commitHash, author };
-	});
-	return committedTodoList;
-}
-
-export class TodoListProvider implements vscode.TreeDataProvider<Dependency> {
-	constructor(private workspaceRoot: string) {}
-
-	getTreeItem(element: Dependency): vscode.TreeItem {
+	getTreeItem(element: TodoTreeItem): TodoTreeItem {
 		return element;
 	}
 
-	getChildren(element?: Dependency): Thenable<Dependency[]> {
-		if (!this.workspaceRoot) {
-			vscode.window.showInformationMessage("No dependency in empty workspace");
-			return Promise.resolve([]);
+	getChildren(element?: TodoTreeItem): TodoTreeItem[] {
+		log.call({ element });
+		const isFirstViewElement = !element;
+
+		const prefixes = ["TODO", "HACK", "FIXME"];
+		const allTodoList = (this.workspaceState.get("todoList") || []) as
+			| CommittedTodoList
+			| undefined;
+		if (!allTodoList) {
+			return [new TodoTreeItem("No todo comments.")];
 		}
 
-		if (element) {
-			return Promise.resolve(
-				this.getDepsInPackageJson(
-					path.join(
-						this.workspaceRoot,
-						"node_modules",
-						element.label,
-						"package.json",
-					),
-				),
-			);
-		}
-		const packageJsonPath = path.join(this.workspaceRoot, "package.json");
-		if (this.pathExists(packageJsonPath)) {
-			return Promise.resolve(this.getDepsInPackageJson(packageJsonPath));
-		}
-		vscode.window.showInformationMessage("Workspace has no package.json");
-		return Promise.resolve([]);
-	}
-
-	/**
-	 * Given the path to package.json, read all its dependencies and devDependencies.
-	 */
-	private getDepsInPackageJson(packageJsonPath: string): Dependency[] {
-		if (this.pathExists(packageJsonPath)) {
-			const toDep = (moduleName: string, version: string): Dependency => {
-				if (
-					this.pathExists(
-						path.join(this.workspaceRoot, "node_modules", moduleName),
-					)
-				) {
-					return new Dependency(
-						moduleName,
-						version,
-						vscode.TreeItemCollapsibleState.Collapsed,
-					);
-				}
-				return new Dependency(
-					moduleName,
-					version,
-					vscode.TreeItemCollapsibleState.None,
+		if (isFirstViewElement) {
+			return prefixes.map((prefix) => {
+				const list = allTodoList.filter((todo) => todo.prefix === prefix);
+				return new TodoTreeItem(
+					prefix,
+					list.length && vscode.TreeItemCollapsibleState.Expanded,
 				);
-			};
-
-			const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
-
-			const deps = packageJson.dependencies
-				? Object.keys(packageJson.dependencies).map((dep) =>
-						toDep(dep, packageJson.dependencies[dep]),
-				  )
-				: [];
-			const devDeps = packageJson.devDependencies
-				? Object.keys(packageJson.devDependencies).map((dep) =>
-						toDep(dep, packageJson.devDependencies[dep]),
-				  )
-				: [];
-			return deps.concat(devDeps);
+			});
 		}
-		return [];
+
+		const specifiedTodoList = allTodoList
+			.filter((todo) => todo.branch === this.currentBranch)
+			.filter((todo) => todo.prefix === element.label);
+		log.call({ specifiedTodoList });
+		return specifiedTodoList.map((todo) => {
+			return new TodoTreeItem(todo.preview);
+		});
 	}
 
-	private pathExists(p: string): boolean {
-		try {
-			fs.accessSync(p);
-		} catch (err) {
-			return false;
+	private _onDidChangeTreeData: vscode.EventEmitter<
+		TodoTreeItem | undefined | null
+	> = new vscode.EventEmitter<TodoTreeItem | undefined | null>();
+	readonly onDidChangeTreeData: vscode.Event<TodoTreeItem | undefined | null> =
+		this._onDidChangeTreeData.event;
+
+	refresh(): void {
+		this._onDidChangeTreeData.fire(undefined);
+		console.log("refreshed");
+	}
+
+	getUnCommitTodoList(): UnCommitTodoList {
+		const cmd = `git -C ${this.workspaceRoot} diff \
+								 | grep -E ${searchWordGit.source} | grep '^+' \
+								 || [ $? = 1 ] # if grep return 1, then return 0`;
+		const output = child_process.execSync(cmd).toString();
+
+		// cut last "\n" and split by "\n"
+		const searchResultArray = output.slice(0, -1).split("\n");
+		const unCommitTodoList = searchResultArray.map((output) => {
+			const matchedWord = output.match(searchWordTS);
+			if (!matchedWord) {
+				throw new ShouldHaveBeenIncludedSearchWordError(output);
+			}
+			return {
+				prefix: matchedWord[0].slice(0, -1),
+				sourcePath: output.slice(1),
+				line: 0,
+				preview: "",
+				isPreview: true,
+			};
+		});
+		return unCommitTodoList;
+	}
+
+	getCommittedTodoList(): CommittedTodoList {
+		const cmd = `cd ${this.workspaceRoot} &&
+								 git -c grep.lineNumber=true grep -E ${searchWordGit.source} $(git branch --format='%(objectname) %(refname:short)' \
+								 | sort | uniq -w 40 | cut -c 42-) \
+								 || [ $? = 1 ] # if grep return 1, then return 0`;
+		const gitGrepResult = child_process.execSync(cmd).toString();
+		if (gitGrepResult === "") {
+			return [];
 		}
-		return true;
+
+		// cut last "\n" and split by "\n"
+		const searchResultArray = gitGrepResult.slice(0, -1).split("\n");
+
+		const committedTodoList = searchResultArray.map((output) => {
+			// tracked file
+			const firstIndex = output.indexOf(":");
+			const secondIndex = output.indexOf(":", firstIndex + 1);
+			const thirdIndex = output.indexOf(":", secondIndex + 1);
+			const fullLine = output.slice(thirdIndex + 1);
+			const matchedWord = fullLine.match(searchWordTS);
+			if (!matchedWord) {
+				throw new ShouldHaveBeenIncludedSearchWordError(output);
+			}
+			const prefix = matchedWord[0].slice(0, -1);
+			const branch = output.slice(0, firstIndex);
+			const sourcePath = output.slice(firstIndex + 1, secondIndex);
+			const line = Number(output.slice(secondIndex + 1, thirdIndex));
+			const preview = fullLine.slice(matchedWord.index);
+
+			const cmd = `cd ${this.workspaceRoot} && git blame ${branch} -L ${line},${line} ${sourcePath}`;
+			const blameOutput = child_process.execSync(cmd).toString();
+
+			// get commit hash, author
+			const commitHash = blameOutput.slice(0, 8);
+			const author = blameOutput.split(" ")[1].slice(1);
+
+			return {
+				prefix,
+				branch,
+				sourcePath,
+				line,
+				preview,
+				isPreview: true,
+				commitHash,
+				author,
+			};
+		});
+		return committedTodoList;
 	}
 }
 
-class Dependency extends vscode.TreeItem {
+class TodoTreeItem extends vscode.TreeItem {
 	constructor(
 		public readonly label: string,
-		private version: string,
-		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly collapsibleState?: vscode.TreeItemCollapsibleState,
 	) {
 		super(label, collapsibleState);
-		this.tooltip = `${this.label}-${this.version}`;
-		this.description = this.version;
 	}
 }
-
 export function deactivate() {}
