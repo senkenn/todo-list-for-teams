91cca60c66a89b556fb7511ce9c01c5e0de60a65
senkenn <senken32@gmail.com>
2024-01-30 02:32:31 +0900

    Refactor Jest test runner in index.ts

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -5,12 +5,35 @@ import type { Config } from "@jest/types";
 
 export async function run(
 	testsRoot: string,
-	clb: (error?: Error, failures?: number) => void,
+	reportTestResults: (error?: Error, failures?: number) => void,
 ): Promise<void> {
 	const projectRootPath = path.resolve(__dirname, "../../../");
 	const config = path.join(projectRootPath, "jest.config.js");
 
 	console.info(`Running Jest tests from ${projectRootPath}...`);
 
-	await runCLI({ config } as Config.Argv, [projectRootPath]);
+	await runCLI({ config } as Config.Argv, [projectRootPath])
+		.then((jestCliCallResult) => {
+			// biome-ignore lint/complexity/noForEach: <explanation>
+			jestCliCallResult.results.testResults.forEach((testResult) => {
+				// biome-ignore lint/complexity/noForEach: <explanation>
+				testResult.testResults
+					.filter((assertionResult) => assertionResult.status === "passed")
+					.forEach(({ ancestorTitles, title, status }) => {
+						console.info(`  ● ${ancestorTitles} > ${title} (${status})`);
+					});
+			});
+
+			// biome-ignore lint/complexity/noForEach: <explanation>
+			jestCliCallResult.results.testResults.forEach((testResult) => {
+				if (testResult.failureMessage) {
+					console.error(testResult.failureMessage);
+				}
+			});
+
+			reportTestResults(undefined, jestCliCallResult.results.numFailedTests);
+		})
+		.catch((errorCaughtByJestRunner) => {
+			reportTestResults(errorCaughtByJestRunner, 0);
+		});
 }
