e02e9fa9d755ea9fed29a3848d4611db0c08aafb
senkenn <senken32@gmail.com>
2024-02-03 14:18:43 +0900

    Remove verbose option from jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 // see https://github.com/microsoft/vscode-test/issues/37#issuecomment-700167820
 
 module.exports = {
-	moduleFileExtensions: ["js"],
 	testMatch: ["<rootDir>/out/test/suite/**.test.js"],
 	testEnvironment: "./src/test/vscode-environment.ts",
-	verbose: true,
 };
